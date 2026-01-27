7ccf92d8b3fd63989e9fe98e394d8e5160e42c6a
Nickolas Cerrone <126223367+nickolas-cerrone@users.noreply.github.com>
2026-01-26 22:16:49 -0500

    Initial commit
    Sets up a Vite react-ts project with eslint + prettier settings enabled for consistent formatting

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 4,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+};
+export default config;
